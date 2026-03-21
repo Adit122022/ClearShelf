25e36d94a15a9271e6b072a4bcac7a154ceb97b0
Adit122022 <adity202212@gmail.com>
2026-03-21 22:31:38 +0530

    clearing repo

--- product-dashboard/src/config/constants.ts
+++
@@ -1,3 +0,0 @@
-export const API_BASE_URL = import.meta.env.API_BASE_URL || "https://dummyjson.com"
-export const CLOUDINARY_CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME || "";
-export const CLOUDINARY_UPLOAD_PRESET = import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET || "";
